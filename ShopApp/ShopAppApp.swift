23cccd3fd05783c3d121ec8c1c4278c356846f56
Anton Khrapov <froe054@mail.ru>
2023-01-03 12:16:29 +0700

    Initial Commit

---
+++ ShopApp/ShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopAppApp.swift
+//  ShopApp
+//
+//  Created by Anton on 03.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
